3979a6bdefb0ad98ef372fd19f821d9239cd4a0c
Gurianov1 <newsaabbb228@yandex.ru>
2023-06-06 21:22:20 +0300

    add

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Gender(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+class Children(models.Model):
+    parent = models.ForeignKey(Person, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    age = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Pet(models.Model):
+    parent = models.ForeignKey(Person, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    age = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
